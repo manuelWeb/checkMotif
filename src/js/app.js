f7ad272505eda7e6165bb5ab7b162f2b749fa088
manuelWeb <devries.webdesigner@gmail.com>
2017-05-20 19:49:27 +0200

    modification fct=>motif passé en arg dans le html

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
 // Place JS here.
 var motif  = /^[0-9]{2}\.[0-9]{2}\.[0-9]{2}$/;
 
-function chk_motif() {
-  var chaine = document.querySelector('.reg_test').value;
+var chk_motif = function (chaine) {
   console.log(
     ` motif: ${motif}, chaine: ${chaine},
     réponse: ${motif.test(chaine)}
@@ -10,10 +9,9 @@ function chk_motif() {
   );
 };
 
+// exo .amp et fct flechée
 var ary = [1,2,3,4,5,6];
-
 // const foisdeux = ary.map(num => num * 2);
 // console.log(foisdeux)
-
 const foisdeux = ary.map((num) => num * 2 +' hello');
-console.log(foisdeux)
+console.log(foisdeux);

dce22fa025a5d2eb335f574bffdaa6dc2e7a51e9
macfly <manuel@webdesignord.fr>
2017-05-26 10:43:44 +0100

    fct à créer touch n pour page next site fotolia

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,9 @@ var ary = [1,2,3,4,5,6];
 // console.log(foisdeux)
 const foisdeux = ary.map((num) => num * 2 +' hello');
 console.log(foisdeux);
+
+// fotolia touch n pour page suivante :
+var pat = window.location.href; var reg = /[0-9]{4}/;
+pat.match(reg)
+// pat.replace(reg,'1700')
+document.location = pat.replace(reg,'2000')
